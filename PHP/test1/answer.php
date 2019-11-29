2458dbd9ca18392bcd669909e5e05f6ac33faf6c
zongxili <zongxiliwork@gmail.com>
2019-11-28 20:11:22 -0800

    init commit with file PHP

---
+++ PHP/test1/answer.php
@@ -0,0 +1,10 @@
+<?php
+  echo "here";
+  include 'input-source.jsx';
+
+  
+  // $myfile = fopen('input-source.jsx', "r") or die("Unable to open file!");
+  // $source = fread($myfile, filesize('input-source.jsx'));
+  // echo $source[1];
+  // fclose($myfile); // good programming practice 
+?>
