d6ad2be9636e179d3c23994050bb0a1198e5938b
zongxili <zongxiliwork@gmail.com>
2019-11-29 11:49:13 -0800

    init for the day

--- PHP/test1/answer.php
+++ PHP/test1/answer.php
@@ -1,18 +1,28 @@
 <?php
   // $answer_file = 'input-source-consolidated.jsx';
   // $myfile = fopen($answer_file, "w");
-  $source = file_get_contents("input-source.jsx");//将整个文件内容读入到一个字符串中
+  $source_file = file_get_contents("input-source.jsx");
   // echo $source;
   // $asArr = explode(' {', $source );
   // echo $asArr;
 
 
   // delete the 1st and last line of the string
-  $result = preg_replace('/^.+\n/', '', $source);
-  $result = preg_replace('/\n}$/', '', $result);
+  $nested = preg_replace('/^.+\n/', '', $source_file);
+  $nested = preg_replace('/\n}$/', '', $nested);
   // echo $result;
 
-  $asArr = explode(': {', $result );
-  echo "afas". $asArr[1];
+
+  // this one get the first element as string,
+  // the rest needs to be take out from other elements
+  $asArr = explode(': {', $nested);
+  // $last_one = $asArr[count($asArr) - 1];
+  
+  foreach( $asArr as $val ){
+    echo $val;
+    echo "\n";
+  }
+
+  // echo $inLastOne[0];
   // echo "\n";
 ?>
