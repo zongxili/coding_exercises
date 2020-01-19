dca956bbcc8b611d70ef14918aaf35263f3d80c2
Zongxi Li <zongxili@Zongxis-MBP.hitronhub.home>
2020-01-19 02:23:35 -0800

    added the total price sale

--- OOP/asg_wineDirect/cashier.js
+++ OOP/asg_wineDirect/cashier.js
@@ -1,11 +1,6 @@
 const inputFile = process.argv[2];
 const fs = require("fs");
 
-let itemsInBill = {};
-let itemArr = [];
-let totalPrice = 0.00;
-let undefinedCode = [];
-
 class singleItemInBill {
 	constructor(quantity, total_price) {
 		this.quantity = quantity;
@@ -13,24 +8,49 @@ class singleItemInBill {
 	}
 }
 
-const item_db = require('./itemDB'); 
+class totalBill {
+	constructor() {
+    this.totalPrice = 0;
+	}
+	addToBill(itemPrice) {
+		this.totalPrice += itemPrice;
+	}
+	getPriceAfterSales() {
+		let discount = Math.floor(this.totalPrice / 100) * 5;
+		return this.totalPrice - discount;
+  }
+}
+
+let itemsInBill = {};
+let itemArr = [];
+let aBill = new totalBill();
+let undefinedCode = [];
+
+const item_db = require('./itemDB'); // import item database from another file
 
 var data = fs.readFileSync(inputFile, 'utf8');
 itemArr = data.toString().split("\n");
 for (let a = 0; a < itemArr.length; a++){ // should try using map
-	itemArr[a] = [parseInt(itemArr[a].split(":")[0]), parseFloat(itemArr[a].split(":")[1])];
-}
+	itemArr[a] = [itemArr[a].split(":")[0], parseFloat(itemArr[a].split(":")[1])];
+} // read the input file
 
 for (let a = 0; a < itemArr.length; a++){
-	if (item_db[parseInt(itemArr[a][0])]){
-		let name = item_db[parseInt(itemArr[a][0])].itemName;
+	if (item_db[itemArr[a][0]]){
+		let name = item_db[itemArr[a][0]].itemName;
 		let totalPriceForSingleItem = item_db[itemArr[a][0]].sales(itemArr[a][1]);
 		itemsInBill[name] = new singleItemInBill(itemArr[a][1], parseFloat(totalPriceForSingleItem.toFixed(2)));
-		totalPrice = totalPrice + totalPriceForSingleItem;
+		aBill.addToBill(totalPriceForSingleItem);
 	}
 	else undefinedCode.push(itemArr[a][0]);
 }
 
+// output part
 console.table(itemsInBill);
-console.log(undefinedCode); // need to format this
-console.log(`Total Price is $${totalPrice}.`);
+if (undefinedCode.length !== 0){
+	process.stdout.write("Undefined Code(s): ");
+	undefinedCode.map(code =>{
+		process.stdout.write(code + ' ');
+	})
+	console.log();
+}
+console.log(`Total Price is $${aBill.getPriceAfterSales()}.`);
--- OOP/asg_wineDirect/itemDB.js
+++ OOP/asg_wineDirect/itemDB.js
@@ -1,21 +1,21 @@
 const item_db = {
-	0001: {
+	"0001": {
 		itemName: "apple",
 		itemCode: 0001,
 		price: 3.00,
 		sales: function(quantity) {
-			return quantity * this.price; // buy one get one free
+			return quantity * this.price;
 		}
 	},
-	0002: {
+	"0002": {
 		itemName: "banana",
 		itemCode: 0002,
 		price: 2.00,
 		sales: function(quantity) {
-			return quantity * this.price; // buy one get one free
+			return quantity * this.price;
 		}
 	},
-	0003: {
+	"0003": {
 		itemName: "blueberry",
 		itemCode: 0003,
 		price: 1.67,
